154e563ef9317f1cbdc0b771a86639a0111eecaf
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2024-05-05 10:08:18 +0530

    Initial commit

---
+++ src/test/java/com/aryak/bookapp/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryak.bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
